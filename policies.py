a6f9fed77b705c351359eb23b7788267265978d3
Mats <mats.maiwald@gmail.com>
2021-01-10 15:25:08 +0100

    Make old policies compatible with new interface

--- policies.py
+++ policies.py
@@ -6,17 +6,17 @@ import random
 
 
 class PDPolicy(ABC):
-    def action(self, last_move_opponent: str):
+    def get_action(self, last_moves_opponent: List[str]):
         raise NotImplementedError
 
 
 class AlwaysCoopPolicy(PDPolicy):
-    def action(self, last_move_opponent: str):
+    def get_action(self, last_moves_opponent: List[str]):
         return "coop"
 
 
 class AlwaysDefectPolicy(PDPolicy):
-    def action(self, last_move_opponent: str):
+    def get_action(self, last_moves_opponent: List[str]):
         return "defect"
 
 
@@ -26,13 +26,13 @@ class ProbabilisticPolicy(PDPolicy):
     def __init__(self, p_defect: float):
         self.p_defect = p_defect
 
-    def action(self, last_move_opponent: str):
+    def get_action(self, last_moves_opponent: List[str]):
         return "defect" if random.random() < self.p_defect else "coop"
 
 
 class TitForTatPolicy(PDPolicy):
-    def action(self, last_move_opponent: str):
-        return "defect" if last_move_opponent == "defect" else "coop"
+    def get_action(self, last_moves_opponent: List[str]):
+        return "defect" if last_moves_opponent[-1] == "defect" else "coop"
 
 
 class GeneralPolicy(PDPolicy):
